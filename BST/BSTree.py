da948fb7a49c3aece8cffec81d899f4e682fc8f4
siuJang <jsw74092@gmail.com>
2024-05-09 15:14:56 +0900

    BST

---
+++ BST/BSTree.py
@@ -0,0 +1,10 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
